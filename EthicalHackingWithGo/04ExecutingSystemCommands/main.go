d34d031ffc1c88e92e89e84211763484456cda19
sk <ya.sk10@yanswx.ru>
2022-07-18 00:40:22 +0300

    study 4 execute system commands

---
+++ EthicalHackingWithGo/04ExecutingSystemCommands/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+
+	command := "date"
+	cmd_obj := exec.Command(command)
+	cmd_obj.Stdout = os.Stdout
+	cmd_obj.Stderr = os.Stderr
+	err := cmd_obj.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
