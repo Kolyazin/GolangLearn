e138930c02431b05779a7d3801ae64508db84975
sk <ya.sk10@yanswx.ru>
2022-07-18 00:45:35 +0300

    study 4 refactoring

--- EthicalHackingWithGo/04ExecutingSystemCommands/main.go
+++ EthicalHackingWithGo/04ExecutingSystemCommands/main.go
@@ -6,15 +6,21 @@ import (
 	"os/exec"
 )
 
-func main() {
-
-	command := "date"
-	cmd_obj := exec.Command(command)
+func executeCommand(command string, args_arr []string) (err error) {
+	args := args_arr
+	cmd_obj := exec.Command(command, args...)
 	cmd_obj.Stdout = os.Stdout
 	cmd_obj.Stderr = os.Stderr
-	err := cmd_obj.Run()
+	err = cmd_obj.Run()
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
+	return nil
+}
 
+func main() {
+
+	command := "ls"
+	executeCommand(command, []string{"-l"})
 }
