adca348571c8d8a163e06f8159dc4f6213a70e4b
sk <ya.sk10@yanswx.ru>
2022-07-25 17:02:52 +0300

    make nmap with github.com/Ullaakut/nmap

---
+++ EthicalHackingWithGo/06CreatingNetworkScanner/go.mod
@@ -0,0 +1,7 @@
+module main
+
+go 1.17
+
+require github.com/Ullaakut/nmap v2.0.2+incompatible
+
+require github.com/stretchr/testify v1.8.0 // indirect
--- EthicalHackingWithGo/06CreatingNetworkScanner/main.go
+++ EthicalHackingWithGo/06CreatingNetworkScanner/main.go
@@ -1,33 +1,51 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net"
-	"sync"
+	"log"
+	"time"
+
+	"github.com/Ullaakut/nmap"
 )
 
-func ScanPort(port int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	IP := "scanme.nmap.org"
+func main() {
+	targetIP := "192.168.10.1/24"
 
-	address := fmt.Sprintf(IP+":%d", port)
-	connection, err := net.Dial("tcp", address)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+
+	defer cancel()
+
+	scanner, err := nmap.NewScanner(
+		nmap.WithTargets(targetIP),
+		nmap.WithPorts("80, 443"),
+		nmap.WithContext(ctx),
+	)
 
 	if err != nil {
-		return
-		//		fmt.Printf("[+] Connection established..PORT %v %s\n", port, connection.RemoteAddr().String())
-	}
-	fmt.Printf("%d is open\n", port)
-	connection.Close()
-}
-
-func main() {
-	var wg sync.WaitGroup
-
-	for i := 1; i < 100; i++ {
-		wg.Add(1)
-		go ScanPort(i, &wg)
+		log.Fatal("error: ", err)
 	}
 
-	wg.Wait()
+	results, warning, err := scanner.Run()
+	if err != nil {
+		log.Fatal("error: ", err)
+	}
+
+	if warning != nil {
+		log.Fatal("error: %s\n", warning)
+	}
+
+	for _, host := range results.Hosts {
+		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+			continue
+		}
+		//		fmt.Printf("IP: %q\n", host.Addresses[0])
+		//		if len(host.Addresses) > 1 {
+		//			fmt.Printf("MAC: %v\n", host.Addresses[1])
+		//		}
+		for _, val := range host.Addresses {
+			fmt.Printf("IP: %q\n", val)
+		}
+	}
+
 }
